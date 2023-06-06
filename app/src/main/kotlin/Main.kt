adebaa83f29f6d5ea09d93ee7a77ac0b19c30c97
Carlos Henrique de Oliveira Evangelista <carloshenrique.o.e@gmail.com>
2023-06-06 11:44:53 -0300

    Initial commit using the Kotlin Language

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var name = "Carlos Oliveira"
+    var text: String
+    val calcado: Byte = 41
+    val pib = 1869000000000
+    val populacao = 211000000
+    println("PIB per capta = ${pib / populacao}")
+}
